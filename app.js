9cf0b8d56e78b6c64b57811c04cb65eebbec241d
Liel Papismadov <liel.papi@gmail.com>
2024-12-09 18:40:33 +0200

    server

---
+++ app.js
@@ -0,0 +1,11 @@
+const initApp = require("./server");
+const port = process.env.PORT;
+
+console.log("1");
+initApp().then((app) => {
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+});
+
+// module.exports = app;
